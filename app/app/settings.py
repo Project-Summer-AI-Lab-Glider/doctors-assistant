3b4d5f62f6325ec3df478ed1733dd2bc27a63c0f
alicjak1519 <akrzeminska1519@gmail.com>
2020-08-05 00:41:07 +0200

    feat: Added create user API

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # dependencies
+    'rest_framework',
+    'rest_framework.authtoken',
+    
+    # apps
+    'core',
+    'user',
 ]
 
 MIDDLEWARE = [
